775477f4c0b04f11d9430965d330692f4d4eb8e9
Srinivasan R <srinivasanr@gmail.com>
2014-12-25 18:06:17 +0530

    Add the random_useragent module with the middleware class
    Also add .idea/ to gitignore file.

---
+++ random_useragent.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*-coding:utf-8-*-
+"""Scrapy Middleware to set a random User-Agent for every Request.
+
+Downloader Middleware which uses a file containing a list of
+user-agents and sets a random one for each request.
+"""
+
+import random
+from scrapy import signals
+from scrapy.contrib.downloadermiddleware.useragent import UserAgentMiddleware
+
+
+class RandomUserAgentMiddleware(UserAgentMiddleware):
+    def __init__(self, settings, user_agent='Scrapy'):
+        super(RandomUserAgentMiddleware, self).__init__()
+        self.user_agent = user_agent
+        user_agent_list_file = settings.get('USER_AGENT_LIST')
+        with open(user_agent_list_file, 'r') as f:
+            self.user_agent_list = [line.strip() for line in f.readlines()]
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        obj = cls(crawler.settings)
+        crawler.signals.connect(obj.spider_opened, signal=signals.spider_opened)
+        return obj
+
+    def process_request(self, request, spider):
+        user_agent = random.choice(self.user_agent_list)
+        if user_agent:
+            request.headers.setdefault('User-Agent', user_agent)
