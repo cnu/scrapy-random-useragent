08042a56c5510d2526f4e13ff9e61ac3f4ecb6f3
Srinivasan R <srinivasanr@gmail.com>
2014-12-25 18:19:23 +0530

    Add Author Information to module
    And PEP-8-ify code.

--- random_useragent.py
+++ random_useragent.py
@@ -10,8 +10,18 @@ import random
 from scrapy import signals
 from scrapy.contrib.downloadermiddleware.useragent import UserAgentMiddleware
 
+__author__ = "Srinivasan Rangarajan"
+__copyright__ = "Copyright 2014, Srinivasan Rangarajan"
+__credits__ = ["Srinivasan Rangarajan"]
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "Srinivasan Rangarajan"
+__email__ = "srinivasanr@gmail.com"
+__status__ = "Development"
+
 
 class RandomUserAgentMiddleware(UserAgentMiddleware):
+
     def __init__(self, settings, user_agent='Scrapy'):
         super(RandomUserAgentMiddleware, self).__init__()
         self.user_agent = user_agent
@@ -22,7 +32,8 @@ class RandomUserAgentMiddleware(UserAgentMiddleware):
     @classmethod
     def from_crawler(cls, crawler):
         obj = cls(crawler.settings)
-        crawler.signals.connect(obj.spider_opened, signal=signals.spider_opened)
+        crawler.signals.connect(obj.spider_opened,
+                                signal=signals.spider_opened)
         return obj
 
     def process_request(self, request, spider):
